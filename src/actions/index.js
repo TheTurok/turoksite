1e7afc2c6adfdc626b356d71db4a7d350a4e062f
Timothy Choi <TurokChoi@gmail.com>
2018-10-31 19:39:55 -0700

    inital stage of mail

--- src/actions/index.js
+++
@@ -1,1 +0,0 @@
-  const API_KEY = 'MNv5cGDRm95PP7Bg7iL7pRmd24ZqAPD6';
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ import '../styles/style.css';
 
 export default class Footer extends Component {
 
-  const API_KEY = 'MNv5cGDRm95PP7Bg7iL7pRmd24ZqAPD6';
   render() {
     return (
       <div id = "foot" className = "col-sm-6 col-sm-offset-3 col-md-8 col-md-offset-2">
