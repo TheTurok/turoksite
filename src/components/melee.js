09d68dbaffabde4e8c115620b60096bdd6858f41
Timothy Choi <TurokChoi@gmail.com>
2018-11-02 17:16:47 -0700

    melee gifs for display

--- src/components/melee.js
+++ src/components/melee.js
@@ -4,8 +4,14 @@ import React, { Component } from 'react';
 export default class Melee extends Component {
   render() {
     return (
-      <div className = "content">
-        This is Melee Gifs!
+      <div className = "content text-center">
+        <img src={require('../melee_gifs/1.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/2.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/3.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/4.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/5.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/6.gif')} alt = "Combo"/>
+        <img src={require('../melee_gifs/7.gif')} alt = "Combo"/>
       </div>
 
     );
