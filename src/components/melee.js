ec9aab7eea1f10ecb2361423e3a64cbacc79df6e
Timothy Choi <TurokChoi@gmail.com>
2018-11-12 15:16:09 -0800

    melee update more files

--- src/components/melee.js
+++ src/components/melee.js
@@ -5,13 +5,21 @@ export default class Melee extends Component {
   render() {
     return (
       <div className = "content text-center">
-        <img src={require('../melee_gifs/1.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/2.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/3.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/4.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/5.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/6.gif')} alt = "Combo"/>
-        <img src={require('../melee_gifs/7.gif')} alt = "Combo"/>
+        <img src={require('../melee/marth_icon.png')} alt = "Combo"/>
+        <img src={require('../melee/falcon_icon.png')} alt = "Combo"/>
+        <img src={require('../melee/falco_icon.png')} alt = "Combo"/>
+
+
+        <img src={require('../melee/m1.gif')} alt = "Combo"/>
+        <img src={require('../melee/m2.gif')} alt = "Combo"/>
+        <img src={require('../melee/m3.gif')} alt = "Combo"/>
+        <img src={require('../melee/m4.gif')} alt = "Combo"/>
+        <img src={require('../melee/m5.gif')} alt = "Combo"/>
+        <img src={require('../melee/m6.gif')} alt = "Combo"/>
+        <img src={require('../melee/m7.gif')} alt = "Combo"/>
+        <img src={require('../melee/f1.gif')} alt = "Combo"/>
+        <img src={require('../melee/f2.gif')} alt = "Combo"/>
+        <img src={require('../melee/f3.gif')} alt = "Combo"/>
       </div>
 
     );
