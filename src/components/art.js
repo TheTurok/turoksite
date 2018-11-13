f9f697129098a84b18755428df3ef15f314b52b4
Timothy Choi <TurokChoi@gmail.com>
2018-11-12 18:50:26 -0800

    website ready to deploy and show

--- src/components/art.js
+++ src/components/art.js
@@ -5,7 +5,14 @@ export default class Art extends Component {
   render() {
     return (
       <div className = "content text-center">
-        This is Art!
+        <img src={require('../art/art1.jpg')} alt = "Art"/>
+        <img src={require('../art/art2.jpg')} alt = "Art"/>
+        <img src={require('../art/art3.jpg')} alt = "Art"/>
+        <img src={require('../art/art4.jpg')} alt = "Art"/>
+        <img src={require('../art/art5.jpg')} alt = "Art"/>
+        <img src={require('../art/art6.jpg')} alt = "Art"/>
+        <img src={require('../art/art7.jpg')} alt = "Art"/>
+        <img src={require('../art/art8.jpg')} alt = "Art"/>
       </div>
 
     );
