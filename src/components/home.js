a726443257832845453dbab9cea22b2d2439d066
Timothy Choi <TurokChoi@gmail.com>
2018-11-15 12:53:23 -0800

    about me

--- src/components/home.js
+++ src/components/home.js
@@ -5,19 +5,19 @@ export default class Home extends Component {
     return (
       <div className = "col-sm-12 col-md-8 col-md-offset-2 content">
         <p>
-          I graduated from Univeristy of California, San Diego 2017 with a B.S. in Computer Science.
-          My focus in software enginering is web development.
-          I chose this specification because it showcased my ability to create content that works smoothly and efficiently for up-and-coming entrepreneurs.
-          I make websites using React becauseit is simple yet it provides a degree of freedom that allows me to express my creative ability while meeting each client's demands.
+          I graduated from the University of California, San Diego in 2017 with a B.S. in Computer Science.
+          Currently, my focus in software engineering is web development because I am able to showcase my ability to create content that works smoothly and efficiently for up-and-coming entrepreneurs while simultaneously helping them grow their businesses.
+          I build websites primarily using React because it is simple, yet it provides a degree of freedom that allows me to express my creative ability while meeting each client's demands.
 
           <br/><br/>
 
-          My passion is to express my creativity because people are able to enjoy this experience.
-          Visual art allowed me express myself through an object.
-          Dancing lets me express myself through my body in every beat.
-          And music let's my creativity flow through people's ears.
-          I love computer science because I am able to create things and people are able to enjoy and experience my work.
-          With web development, I believe I can express myself in all ways a human can desire.
+
+          <br/><br/>
+
+          My passion is to express my creativity through any means because people are able to experience, enjoy, and hopefully be inspired by whatever I create. 
+          Visual art allows me to express myself through a physical art medium, dancing allows me to express myself through my body in every beat, and music lets my creativity flow through people's ears.
+          Computer science is another passion of mine because I am similarly able to create things that people are able to enjoy and experience.
+          With web development specifically, I can transform a client's vision, incorporate my creativity, and express ideas in clear and meaningful ways.
 
           <br/><br/>
 
