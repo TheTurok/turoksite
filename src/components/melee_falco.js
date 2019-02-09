8571ed3f106f06502a1602128d02f840c3d0afca
Timothy Choi <TurokChoi@gmail.com>
2019-02-08 21:36:10 -0800

    falco got one combo in its library now

--- src/components/melee_falco.js
+++ src/components/melee_falco.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import {browserHistory} from 'react-router';
+import {Link} from 'react-router-dom';
+import aLeft from '../melee/aLeft.png';
+import aRight from '../melee/aRight.png';
 
 
-export default class MeleeFalcon extends Component {
+export default class MeleeFalco extends Component {
+  onNextButton(){
+    this.props.match.params.id++;
+
+    if(this.props.match.params.id === 2)
+      this.props.match.params.id = 1;
+
+    this.props.history.push(`/melee/falco/${this.props.match.params.id}`);
+  }
+
+  onPrevButton(){
+    this.props.match.params.id--;
+
+    if(this.props.match.params.id === 0)
+      this.props.match.params.id = 1;
+
+    this.props.history.push(`/melee/falco/${this.props.match.params.id}`);
+  }
+
   render() {
+
+
     return (
       <div className = "content text-center">
-        Coming Soon!
+        <img src= {aLeft}
+          onClick={this.onPrevButton.bind(this)}
+          className="img-responsive" alt = "arrowBtn"/>
+
+
+        <img src={require(`../melee/b${this.props.match.params.id}.gif`)}
+          className="img-responsive" alt = "Combo"/>
+
+
+        <img src={aRight}
+          onClick={this.onNextButton.bind(this)}
+          className="img-responsive" alt = "arrowBtn"/>
       </div>
 
     );
