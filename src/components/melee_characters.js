e74666fa1278beb5c0e33e04f9ed196237ef1d39
Timothy Choi <TurokChoi@gmail.com>
2019-10-30 20:58:30 -0700

    set state for melee page

--- src/components/melee_characters.js
+++ src/components/melee_characters.js
@@ -3,45 +3,66 @@ import aLeft from '../melee/aLeft.png';
 import aRight from '../melee/aRight.png';
 import {Link} from 'react-router-dom';
 
-let file_count;
-
 export default class MeleeCharacters extends Component {
-  render() {
+  constructor(props){
+    super(props);
+
     let {
       character, id
     } = this.props.match.params;
 
-    let num = Number(id);
+    this.state = {
+      file_count: null,
+      id: Number(id),
+      character: character
+    }
+  }
 
-    switch(character){
+  componentWillMount(){
+    switch(this.state.character){
       case 'falco':
-        file_count = 6;
+        this.setState({file_count:6});
         break;
       case 'falcon':
-        file_count = 3;
+        this.setState({file_count:3});
         break;
       case 'marth':
-        file_count = 8;
+        this.setState({file_count:8});
         break;
       default:
-        file_count = 1;
+        this.setState({file_count:2});
     }
-    if(num > file_count-1)
-      num = 0;
-    if(num < 0)
-      num = file_count-1;
+  }
 
+  retrieveGIF(c, i){
+    let num = i;
+    if (num > this.state.file_count -1){
+      this.setState({id:0});
+      num = 0;
+    }
+    if (num < 0){
+      this.setState({id:this.state.file_count -1});
+      num = this.state.file_count - 1;
+    }
+
+    return(
+      <img src={require(`../melee/${c}${num}.gif`)}
+        className="col-md-8 img-responsive" alt = "Combo"/>
+    );
+  }
+
+
+  render() {
     return (
       <div className = "content text-center container-fluid row">
 
-        <Link to = {`/melee/${character}/${num-1}`} className="col-md-1 col-md-offset-1" >
+        <Link to = {`/melee/${this.state.character}/${this.state.num-1}`} onClick={() => this.setState({id:this.state.id - 1})} className="col-md-1 col-md-offset-1" >
           <img src= {aLeft} className="img-responsive" alt = "arrowBtn"/>
         </Link>
 
-        <img src={require(`../melee/${character}${num}.gif`)}
-          className="col-md-8 img-responsive" alt = "Combo"/>
+        {this.retrieveGIF(this.state.character, this.state.id)}
 
-        <Link to = {`/melee/${character}/${num+1}`} className="col-md-1" >
+        <Link to = {`/melee/${this.state.character}/${this.state.num+1}`} onClick={() => this.setState({id:this.state.id + 1})} className="col-md-1" >
           <img src= {aRight} className="img-responsive" alt = "arrowBtn"/>
         </Link>
       </div>
---
+++ src/components/melee_charactersss.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import aLeft from '../melee/aLeft.png';
+import aRight from '../melee/aRight.png';
+import {Link} from 'react-router-dom';
+
+let file_count;
+
+export default class MeleeCharacters extends Component {
+  render() {
+    let {
+      character, id
+    } = this.props.match.params;
+
+    let num = Number(id);
+
+    switch(character){
+      case 'falco':
+        file_count = 6;
+        break;
+      case 'falcon':
+        file_count = 3;
+        break;
+      case 'marth':
+        file_count = 8;
+        break;
+      default:
+        file_count = 1;
+    }
+    if(num > file_count-1)
+      num = 0;
+    if(num < 0)
+      num = file_count-1;
+
+    console.log(num);
+
+    return (
+      <div className = "content text-center container-fluid row">
+
+        <Link to = {`/melee/${character}/${num-1}`} className="col-md-1 col-md-offset-1" >
+          <img src= {aLeft} className="img-responsive" alt = "arrowBtn"/>
+        </Link>
+
+        <img src={require(`../melee/${character}${num}.gif`)}
+          className="col-md-8 img-responsive" alt = "Combo"/>
+
+        <Link to = {`/melee/${character}/${num+1}`} className="col-md-1" >
+          <img src= {aRight} className="img-responsive" alt = "arrowBtn"/>
+        </Link>
+      </div>
+
+    );
+  }
+}
